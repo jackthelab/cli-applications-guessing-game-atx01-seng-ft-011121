847110500aa2b015e56a87d617e6dbb6a74173d6
Jacob Hampton <jhampton792@gmail.com>
2021-01-10 19:10:07 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,5 +2,18 @@
 def run_guessing_game
   
   comp_num = rand(1..6).to_s
+  
+  puts "Guess a number from 1 thru 6."
+  
+  input = gets.chomp
+  
+  if input == comp_num
+    print "you guessed the correct number"
+  elsif input == "exit"
+    print "Goodbye!"
+  else
+    print "Sorry! The computer guessed #{comp_num}."
+  end
+  
 end
   
